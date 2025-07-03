debb7b5ec77207e7fcb3dd8bacadac8f711df916
Nutthawut🤖 <122816536+oat1122@users.noreply.github.com>
2025-07-03 12:49:29 +0700

    Add schedule frequency for update command

--- tnp-backend/app/Console/Kernel.php
+++ tnp-backend/app/Console/Kernel.php
@@ -12,7 +12,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('customer:update-customer-manage-by')->runInBackground();
+        $schedule
+            ->command('customer:update-customer-manage-by')
+            ->daily()
+            ->runInBackground();
     }
 
     /**
